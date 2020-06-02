efb33064a8841815b31a08122c89d6e4f7f516cb
Steven J. Selcuk <stevenjselcuk@gmail.com>
2020-06-02 20:00:17 +0300

    Initial Commit

---
+++ Pomosh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pomosh
+//
+//  Created by Steven J. Selcuk on 2.06.2020.
+//  Copyright © 2020 Steven J. Selcuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
