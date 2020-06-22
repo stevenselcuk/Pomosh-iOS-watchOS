d4e51613420bb8ba29fa9da60ebb95c07d59bfb3
Steven J. Selcuk <stevenjselcuk@gmail.com>
2020-06-22 22:57:30 +0300

    ⌚️ Apple watch for 🍅

---
+++ Pomosh Watch Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  Pomosh Watch Extension
+//
+//  Created by Steven J. Selcuk on 3.06.2020.
+//  Copyright © 2020 Steven J. Selcuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
