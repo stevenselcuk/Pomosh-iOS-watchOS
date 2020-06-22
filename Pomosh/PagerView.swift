e636bcb86b759fd338179219b4f306581285da38
Steven J. Selcuk <stevenjselcuk@gmail.com>
2020-06-22 22:56:40 +0300

    🔥 Animated Pager

---
+++ Pomosh/PagerView.swift
@@ -0,0 +1,21 @@
+//
+//  PagerView.swift
+//  Pomosh
+//
+//  Created by Steven J. Selcuk on 2.06.2020.
+//  Copyright © 2020 Steven J. Selcuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct PagerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PagerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PagerView()
+    }
+}
